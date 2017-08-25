1911d318c66a7d5d22f71c8612da19a362c57ebe
Andy Nicholson <andrew@anicholson.net>
2017-08-25 12:55:29 +1000

    Added a pack that handles CSS compilation outside of sprockets. Add Bulma for styling

--- app/javascript/packs/Main.elm
+++ app/javascript/packs/Main.elm
@@ -19,10 +19,9 @@ init =
 
 view : Model -> Html Message
 view model =
-  -- The inline style is being used for example purposes in order to keep this example simple and
   -- avoid loading additional resources. Use a proper stylesheet when building your own app.
   h1 [style [("display", "flex"), ("justify-content", "center")]]
-     [text "Hello Elm!"]
+     [text "Hello Cogent!"]
 
 -- MESSAGE
 
